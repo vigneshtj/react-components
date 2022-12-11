e940989d9d11e2ea68116d27414cefefcd76a7a2
vigneshtj <vignesh.janarthanan@gmail.com>
2022-12-11 16:55:29 +0000

    Fix for infinite re-rendering
    The infinite re-rendering has occurred while adding function as dependency for useEffect.
    This error has been handled with useRef, uselayout along with a cleanup function.
    Best example for handling functional dependency for useEffect. Thanks Jack Herrington!!

--- src/Components/CustomHooks/DataSource/useDataSource.js
+++ src/Components/CustomHooks/DataSource/useDataSource.js
@@ -1,12 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
 
 function useDataSource(dataSource) {
   const [data, setData] = useState(null);
 
+  //Function to handle infinate rerendering while passing function as dependency for useEffect
+  //Step 1: Remove function from dependency for useEffect
+  //Step 2: Handle function updates using useRef and useLayoutEffect as given below
+
+  const useCallBackRef = (callback) => {
+    const callBackRef = useRef(callback);
+
+    useLayoutEffect(() => {
+      callBackRef.current = callback;
+    }, [callback]);
+
+    return callBackRef;
+  };
+
+  const dataSourceFunc = useCallBackRef(dataSource);
+
   useEffect(() => {
     (async () => {
-      const result = await dataSource();
-      setData(result);
+      let isCancelled = false;
+      const result = await dataSourceFunc.current?.();
+      !isCancelled && setData(result);
+
+      //Clean up function if previous fetch is cancelled
+      return () => {
+        isCancelled = true;
+      };
     })();
   }, []);
 
