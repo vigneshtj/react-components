01b00fa500d23195bd83e743911d1861485259a8
vigneshtj <vignesh.janarthanan@gmail.com>
2022-12-13 18:51:02 +0000

    reverted infinite rendering fix

--- src/Components/CustomHooks/DataSource/useDataSource.js
+++ src/Components/CustomHooks/DataSource/useDataSource.js
@@ -1,37 +1,12 @@
-import { useEffect, useState, useRef, useLayoutEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useDataSource(dataSource) {
   const [data, setData] = useState(null);
 
-  /*
-   * Function to handle infinate rerendering while passing function as dependency for useEffect
-   * Step 1: Remove function from dependency for useEffect
-   * Step 2: Handle function updates using useRef and useLayoutEffect as given below
-   * Step 3: Add cleanup function to handle when component is handled due to network delay
-   */
-
-  const useCallBackRef = (callback) => {
-    const callBackRef = useRef(callback);
-
-    useLayoutEffect(() => {
-      callBackRef.current = callback;
-    }, [callback]);
-
-    return callBackRef;
-  };
-
-  const dataSourceFunc = useCallBackRef(dataSource);
-
   useEffect(() => {
     (async () => {
-      let isCancelled = false;
-      const result = await dataSourceFunc.current?.();
-      !isCancelled && setData(result);
-
-      //Clean up function if previous fetch is cancelled
-      return () => {
-        isCancelled = true;
-      };
+      const result = await dataSource();
+      setData(result);
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
